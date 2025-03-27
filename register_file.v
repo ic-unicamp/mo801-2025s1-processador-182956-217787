31ac56fab0467b3d3dd37e5fbbe014a4d9c3e455
Luis Felipe <l182956@dac.unicamp.br>
2025-03-27 13:38:21 -0300

    Fix register_file

--- register_file.v
+++ register_file.v
@@ -1,13 +1,12 @@
 module register_file (
     input           clk,
-    input           rst,
+    input           rst,  // Active-low reset
     input           write_enable_3,
     input   [4:0]   rs1, rs2, rd,
     input   [31:0]  write_data_3,
     output  [31:0]  rd1, rd2
 );
     
-    // register bank
     parameter NREGISTER = 32;
     reg [31:0] register [NREGISTER - 1: 0];
 
@@ -15,15 +14,17 @@ module register_file (
     assign rd2 = (rs2 == 5'b0) ? 32'b0 : register[rs2];
     
     integer i;
-    always @(posedge clk or posedge rst)
+    always @(posedge clk or negedge rst)
     begin
         if (rst == 1'b0)
-        begin
-            for (i = 0; i < 32; i = i + 1)
-                register[i] = 32'b0;
-        end
-
-        if (write_data_3 && rd != 5'b0) register[write_data_3] = rd;
+            begin
+                for (i = 0; i < 32; i = i + 1)
+                    register[i] <= 32'b0;
+            end
+        else if (write_enable_3 && rd != 5'b0)
+            begin
+                register[rd] <= write_data_3;
+            end
     end
 
 endmodule

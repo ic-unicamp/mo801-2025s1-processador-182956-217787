36dcff4b23951934f2b73e17cdc27c2019faed94
Luis Felipe <l182956@dac.unicamp.br>
2025-03-27 13:54:55 -0300

    Refactor mux3 to use explicit binary select logic with default case

--- mux3.v
+++ mux3.v
@@ -3,8 +3,10 @@ module mux3 (
     input   [31:0]  src0, src1, src2,
     output  [31:0]  dest   
 );
-    assign dest =  select[1] ? src2 : 
-                   select[0] ? src1 : 
-                               src0;
+    assign dest = (select == 2'b00) ? src0 :
+                  (select == 2'b01) ? src1 :
+                  (select == 2'b10) ? src2 :
+                                      32'bz;  // undefined for 2'b11
+
 
 endmodule

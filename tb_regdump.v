fd2a297f889c4ba2bf53af6be4617a485db57c68
Luis Felipe <l182956@dac.unicamp.br>
2025-04-10 20:43:09 -0300

    Increase simulation timeout duration and add test for invalid instruction

--- tb_regdump.v
+++ tb_regdump.v
@@ -81,7 +81,7 @@ module tb_regdump();
     $display("*** Starting Register Dump Simulation ***");
 
     // Set a timeout for the simulation
-    #200 $display("Simulation Timeout Reached."); $finish;
+  #20000 $display("Simulation Timeout Reached."); $finish;
   end
 
   // Clock generator
